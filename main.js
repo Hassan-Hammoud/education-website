ff09a69130ae2d39cd72cf3e510357ea5ba818f0
Hassan-Hammoud <hassanhammoud2000@gmail.com>
2024-10-04 20:13:17 +0300

    finished Faqs section

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ window.addEventListener("scroll", () => {
 });
 
 // End Change navbar styles on scroll
+
+// Start Show / Hide Faq Answer
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    // Change Icon
+    const icon = faq.querySelector(".faq__icon i");
+    if (icon.className === "uil uil-plus") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
+});
+// End Show / Hide Faq Answer
